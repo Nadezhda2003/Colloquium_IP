bf48a2afc5e6d9fc59a4237370f7facb2b021808
Nadezhda Bazhanova <nadia.bazhanova@gmail.com>
2022-11-09 11:09:31 +0300

    add few tests

--- src/main/java/com/example/calculatorwebservice/CalulatorFacade.java
+++ src/main/java/com/example/calculatorwebservice/CalulatorFacade.java
@@ -1,4 +1,4 @@
-/*package com.example.calculatorwebservice;
+package com.example.calculatorwebservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
@@ -46,4 +46,3 @@ public class CalulatorFacade {
         return result;
     }
 }
-*/
